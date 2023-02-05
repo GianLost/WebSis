f89165443cf9c4828b24b338ac1943ef0ca253f5
GianL <gianluca19993m@gmail.com>
2023-02-05 16:51:00 -0300

    Alterações e comentários no método de login que controla as rotas para o acesso de usuário

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -45,51 +45,55 @@ namespace WebSis.Controllers
 
                 Users u = new Users();
 
-                WebSisContext db = new WebSisContext();
+                WebSisContext dataBase = new WebSisContext();
 
-                IQueryable<Users> userFound = db.Users.Where(u => u.Login == login || u.Password == password);
+                IQueryable<Users> userFound = dataBase.Users.Where(u => u.Login == login || u.Password == password); // Busca na tabela de registro de usuários o usuário correspondente à pesquisa que compara login e senha informados pelo usuário no formulário de login.
 
-                int secretaryOfUser = userFound.Select(u => u.SecretariesId).First();
+                int secretaryOfUser = userFound.Select(u => u.SecretariesId).First(); // scretaryOfUser recebe uma seleção dentro do objeto query de user found que busca o Id da secretaria relacionada ao usuário.
 
-                string LoginUser = userFound.Select(u => u.Login).FirstOrDefault();
+                string LoginUser = userFound.Select(u => u.Login).First(); // seleciona através do objeto de pesquisa userFound o campo de Login que corresponde ao digitado pelo usuário vereficando se ele existe ou não.
 
-                string PasswordUser = userFound.Select(u => u.Password).First();
+                string PasswordUser = userFound.Select(u => u.Password).First(); // seleciona através do objeto de pesquisa userFound o campo de senha que corresponde ao digitado pelo usuário vereficando se ele existe ou não.
 
-                string EncryptedPassword = Cryptography.EncryptedText(password);
+                string EncryptedPassword = Cryptography.EncryptedText(password); // Transforma em Hash MD5 a string digitada no campo de senha no momento do login;
 
-                if ( secretaryId == -1)
+                if (secretaryId == -1)
                 {
+                    // O valor -1 é atribuido ao campo do select que está selecionado para que seja verificado se o usuário selecionou ou não alguma secretaria. O combobox select é reponsável por conter os dados das secretarias que o usuário deve selecionar para realizar o login caso a estrutura de verificação retorne true a seção será limpa e o usuário terá uma mensagem de erro renderizada no formulário, informando que uma secretaria precisa ser selecionada para continuar com o login.
+
                     ViewData["ErrorLogin"] = "Selecione sua secretaria !";
                     HttpContext.Session.Clear();
-                    return View(); 
+                    return View();
                 }
-                
+
                 if (secretaryOfUser != secretaryId)
                 {
-                    
+                    // Após verificar se uma secretaria foi selecionada, caso a primeira estrutura retorne false ela passa para a verificação que irá validar se a secretaria selecionada corresponde à secretaria que o usuário pertence. Caso a estrutura retorne true uma mensagem de erro será renderizada no formulário, informando que a secretaria selecionada não pertence à aquele usuário.
+
                     ViewData["ErrorLogin"] = "A secretaria selecionada não corresponde a este usuário !";
                     HttpContext.Session.Clear();
-                    return View(); 
-                    
+                    return View();
+
                 }
-                
+
                 if (LoginUser != login || PasswordUser != EncryptedPassword)
                 {
+                    // Após verificar que o usuário selecionou uma secretaria e que a secretaria realmente corresponde á sua secretaria, o método verifica se os dados de login e senha informados existem no banco e se correspondem realmente à um usuário registrado, caso a verificação retorne true uma mensagem de erro será renderizada no formulário, informando que os dados de login ou senha estão incorretos pois não existem na base de dados.
                     ViewData["ErrorLogin"] = "Login ou senha inválidos !";
                     HttpContext.Session.Clear();
                     return View();
-                    
 
-                } 
-                
-                else
+
+                }else
                 {
+                    // Caso as validações sejam satisfeitas o usuário obtém o próximo nível de acesso podendo adentrar à página inicial do sistema que recebe uma validação que verifica se o usuário estabeleceu uma conexão confiável e de acordo com as regras estabelecidas.
                     return RedirectToAction("Index", "Home");
                 }
 
             }
             catch (Exception e)
             {
+                // Caso uma execessão seja gerada um log é criado contendo as informações da excessão que é capturada plo objeto _ILogger e uma mensagem é gerada no console informando o erro gerado e contendo as informações configuradas no objeto de geração de Logs.
 
                 _logger.LogError("Erro ao Realizar Login!" + e.Message);
                 return RedirectToAction("Login", "Home");
